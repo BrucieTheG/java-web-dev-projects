2a0d891e2f62090c24a1148a85be8f7d4550ed3b
Brody <Brody@Brantleas-MacBook-Pro.local>
2023-11-30 18:17:31 -0600

    Bonus Checks Passed

--- datatypes/datatypes-studio/src/main/PiCircle.java
+++ datatypes/datatypes-studio/src/main/PiCircle.java
@@ -3,10 +3,20 @@ public class PiCircle {
     public static void main(String[] args){
         int radius;
         double area;
+        int inputCheck = 0;
         Scanner Input = new Scanner(System.in);
         System.out.println("Enter the radius of the circle : ");
-        radius = Input.nextInt();;
-        area = (radius*radius)*Math.PI;
-        System.out.println("Area of the circle is : " + area);
+        radius = Input.nextInt();
+        while (inputCheck <= 0) {
+            if (radius < 0) {
+                System.out.println("Please enter a positive number");
+                System.out.println("Enter the radius of the circle : ");
+                radius = Input.nextInt();
+            } else {
+                area = (radius * radius) * Math.PI;
+                System.out.println("Area of the circle is : " + area);
+                inputCheck = 1;
+            }
+        }
     }
 }
