99b7b702b1921a82426d9b546fd98f0cb04ff459
Brody <Brody@Brantleas-MacBook-Pro.local>
2023-11-30 18:20:25 -0600

    Logic Fixed

--- datatypes/datatypes-studio/src/main/PiCircle.java
+++ datatypes/datatypes-studio/src/main/PiCircle.java
@@ -5,12 +5,12 @@ public class PiCircle {
         double area;
         int inputCheck = 0;
         Scanner Input = new Scanner(System.in);
-        System.out.println("Enter the radius of the circle : ");
+        System.out.print("Enter the radius of the circle : ");
         radius = Input.nextInt();
         while (inputCheck <= 0) {
             if (radius < 0) {
-                System.out.println("Please enter a positive number");
-                System.out.println("Enter the radius of the circle : ");
+                System.out.println("Please enter a positive number\n");
+                System.out.print("Enter the radius of the circle : ");
                 radius = Input.nextInt();
             } else {
                 area = (radius * radius) * Math.PI;
