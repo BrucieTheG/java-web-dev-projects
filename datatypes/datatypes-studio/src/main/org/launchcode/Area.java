13ce8f517d5a69e8c3f184629ece760225a27d43
Brody <Brody@Brantleas-MacBook-Pro.local>
2023-11-30 18:49:07 -0600

    fixed

--- datatypes/datatypes-studio/src/main/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/org/launchcode/Area.java
@@ -1,20 +1,28 @@
+package org.launchcode;
+
 import java.util.Scanner;
-public class PiCircle {
+public class Area {
     public static void main(String[] args){
         int radius;
         double area;
         int inputCheck = 0;
         Scanner Input = new Scanner(System.in);
-        System.out.print("Enter the radius of the circle : ");
+        System.out.print("Enter circle radius : ");
         radius = Input.nextInt();
-        while (inputCheck <= 0) {
+
+        while (inputCheck == 0) {
+            if (!Input.hasNextInt()) {
+                System.out.println("Please enter a positive number\n");
+                System.out.print("Enter circle radius : ");
+                radius = Input.nextInt();
+            }
             if (radius < 0) {
                 System.out.println("Please enter a positive number\n");
-                System.out.print("Enter the radius of the circle : ");
+                System.out.print("Enter circle radius : ");
                 radius = Input.nextInt();
             } else {
                 area = (radius * radius) * Math.PI;
-                System.out.println("Area of the circle is : " + area);
+                System.out.println("Circle Area : " + area);
                 inputCheck = 1;
             }
         }
