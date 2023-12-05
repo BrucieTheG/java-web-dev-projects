287ac1b49851e85b8dc3835f51e4dda5ada6e126
Brody <Brody@Brantleas-MacBook-Pro.local>
2023-12-04 20:00:32 -0600

    Final

--- control-flow-and-collections/studio/countingclass.java
+++ control-flow-and-collections/studio/countingclass.java
@@ -4,15 +4,22 @@ import java.util.Scanner;
 
 public class countingclass {
     public static void main(String[] args) {
-        HashMap<String, Double> myString = new HashMap<>();
-        Scanner input = new Scanner(System.in);
-        String newString;
-        do {
-        System.out.println("Enter String:");
-            newString = input.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        scanner.close();
 
+        input = input.replaceAll("[^a-zA-Z]", "").toLowerCase();
 
-            char[] charactersInString = myString.toCharArray();
+        char[] characters = input.toCharArray();
+        Map<Character, Integer> charCount = new HashMap<>();
 
+        for (char c : characters) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : charCount.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
 }
