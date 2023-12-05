e13cc5812e97e0b1b9bf7973a1782135f4d81229
Brody <Brody@Brantleas-MacBook-Pro.local>
2023-12-04 19:40:03 -0600

    Test

---
+++ control-flow-and-collections/studio/countingclass.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class countingclass {
+    public static void main(String[] args) {
+        HashMap<String, Double> myString = new HashMap<>();
+        Scanner input = new Scanner(System.in);
+        String newString;
+        do {
+        System.out.println("Enter String:");
+            newString = input.nextLine();
+
+
+            char[] charactersInString = myString.toCharArray();
+
+    }
+}
--- datatypes/datatypes-studio/src/main/org/launchcode/studios/Area.java
+++ datatypes/datatypes-studio/src/main/org/launchcode/studios/Area.java
@@ -1,27 +1,21 @@
-package org.launchcode;
+package org.launchcode.studios;
 
 import java.util.Scanner;
 public class Area {
     public static void main(String[] args){
         int radius;
-        double area;
         int inputCheck = 0;
         Scanner Input = new Scanner(System.in);
         System.out.print("Enter circle radius : ");
         radius = Input.nextInt();
 
         while (inputCheck == 0) {
-            if (!Input.hasNextInt()) {
-                System.out.println("Please enter a positive number\n");
-                System.out.print("Enter circle radius : ");
-                radius = Input.nextInt();
-            }
             if (radius < 0) {
                 System.out.println("Please enter a positive number\n");
                 System.out.print("Enter circle radius : ");
                 radius = Input.nextInt();
             } else {
-                area = (radius * radius) * Math.PI;
+                double area = (radius * radius) * Math.PI;
                 System.out.println("Circle Area : " + area);
                 inputCheck = 1;
             }
